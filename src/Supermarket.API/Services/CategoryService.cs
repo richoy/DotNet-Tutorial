7e78310299a9d2e68015e2f4464d99f6204ff5d6
Ricardo Choy <rchoy@cecropiasolutions.com>
2020-07-24 19:56:04 -0500

    Step 4, implementing categories services

---
+++ src/Supermarket.API/Services/CategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Services;
+
+namespace Supermarket.API.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+        }
+    }
+}
